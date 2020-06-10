89ac216ad743137da68d2c8b4d07d24a17240fb7
aroop <aroopkumarofficial@gmail.com>
2020-06-10 19:25:10 +0530

    Summarize str removed

--- summarize.py
+++ summarize.py
@@ -3,7 +3,7 @@ from gensim.summarization.summarizer import summarize
 
 def summarizer(text):
     try:
-        summary = summarize(str(text), word_count=100)
+        summary = summarize(text, word_count=100)
         if summary:
             return summary
     except Exception as e:
